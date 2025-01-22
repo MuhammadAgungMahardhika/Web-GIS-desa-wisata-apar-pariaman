adf6d6ee97df5932cc87b0976e05831375084909
agung <m.agungmahardika12@gmail.com>
2025-01-22 15:33:18 +0700

    footer

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -2,7 +2,7 @@
   <footer>
       <div class="footer clearfix mb-0 text-muted">
           <div class="float-start">
-              <p>2021 &copy; Muhammad Agung Mahardhika</p>
+              <p>2025 &copy; Muhammad Agung Mahardhika</p>
           </div>
           <div class="float-end">
               <p>keep in touch <i class="fa fa-envelope"></i> <a href="mailto:m.agungmahardika12@gmail.com"> m.agungmahardika12@gmail.com</a></p>
